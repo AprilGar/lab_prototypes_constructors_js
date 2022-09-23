f2eb0d31a4e442321b2be2d125f47042a535e343
April Garlington <april.garlington@hotmail.co.uk>
2022-09-23 14:35:11 +0100

    wrote a test

--- LAB_tests.js
+++ LAB_tests.js
@@ -1,5 +1,12 @@
-test('can get car manufacturer', () => {
+//import so I have access to methods 
+const car = require("./Car.js")
+
+describe('testing car properties can be accessed', ()=> {
+    
+    test('can get car manufacturer', () => {
     expected = 'Mazda';
-    actual = 
+    actual = car.getManufacturer(car1);
     expect(actual).toBe(expected);
+    });
+    
 });
