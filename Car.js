538f4ecbb5a9212b7eaea6918fb1acbf85241464
April Garlington <april.garlington@hotmail.co.uk>
2022-09-23 13:59:23 +0100

    files set up

---
+++ Car.js
@@ -0,0 +1,19 @@
+const Car = function (manufacturer, price, engineType){
+    this.manufacturer = manufacturer;
+    this.price = price;
+    this.engineType = engineType;
+
+    this.getManufacturer = ()=> this.manufacturer;
+    this.setManufacturer = (manufacturer) => this.manufacturer = manufacturer;
+    this.getPrice = () => this.price;
+    this.setPrice = (price) => this.price = price;
+    this.getEngineType = () => this.engineType;
+    this.setEngineType = (engineType) => this.engineType = engineType;
+
+}
+
+const car1 = new Car('Mazda', 15000, 2);
+const car2 = new Car('Vauxhall', 2000, 1.2);
+
+// console.log(car2);
+// console.log(car1.getManufacturer());
---
+++ LAB_tests.js
@@ -0,0 +1,5 @@
+test('can get car manufacturer', () => {
+    expected = 'Mazda';
+    actual = 
+    expect(actual).toBe(expected);
+});
